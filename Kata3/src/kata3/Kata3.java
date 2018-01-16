dbce7921e725c0afbadcb6065efc067d701ee465
zegre <zegre@10.230.172.199>
2018-01-16 15:46:48 +0000

    primera versión de la kata3

---
+++ Kata3/src/kata3/Kata3.java
@@ -0,0 +1,14 @@
+
+package kata3;
+
+public class Kata3 {
+
+   
+    public static void main(String[] args) {
+        HistogramDisplay histo= new HistogramDisplay();
+        histo.execute();
+        
+        
+    }
+    
+}
