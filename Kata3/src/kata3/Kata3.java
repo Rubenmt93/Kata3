0861f17eb97cbbba9b3c1e9c885f02ecd804de96
zegre <zegre@10.230.172.199>
2018-01-16 16:09:08 +0000

    segunda versión de la kata3

--- Kata3/src/kata3/Kata3.java
+++ Kata3/src/kata3/Kata3.java
@@ -5,9 +5,19 @@ public class Kata3 {
 
    
     public static void main(String[] args) {
-        HistogramDisplay histo= new HistogramDisplay();
-        histo.execute();
-        
+       
+        Histogram<String> histogram = new Histogram<>();
+        histogram.increment("gmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("yahoo.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("hotmail.com");
+        histogram.increment("yahoo.es");
+        new HistogramDisplay(histogram).execute();
         
     }
     
